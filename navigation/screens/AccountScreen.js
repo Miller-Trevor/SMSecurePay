ec00e9d0818f6dd14968fe5f7b2f8c69b4a03d3a
Miller-Trevor <131202550+Miller-Trevor@users.noreply.github.com>
2023-09-22 14:54:51 -0400

    Update AccountScreen.js - Merged Buyer and Seller account

--- navigation/screens/AccountScreen.js
+++ navigation/screens/AccountScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -17,21 +17,8 @@ export default function AccountScreen({navigation}) {
                 flexDirection:'row', 
                 justifyContent: 'space-between',
                 }}>
-                <Text style={{fontWeight:'bold', fontSize:18, color: '#fff', fontFamily:'Thonburi-Bold'}}>Create Buyer Account</Text>
+                <Text style={{fontWeight:'bold', fontSize:18, color: '#fff', fontFamily:'Thonburi-Bold'}}>Login to Account</Text>
                 <Ionicons name="cash-outline" size={22} color="#fff"/>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('SellerAccount')}
-                style={{
-                backgroundColor: '#42EC95', 
-                padding: 20,
-                width:270,
-                borderRadius:5, 
-                flexDirection:'row', 
-                justifyContent: 'space-between',
-                }}>
-                <Text style={{fontWeight:'bold', fontSize:18, color: '#fff', fontFamily:'Thonburi-Bold'}}>Create Seller Account</Text>
-                <Ionicons name="pricetag-outline" size={22} color="#fff"/>
             </TouchableOpacity>
         </SafeAreaView>
     );

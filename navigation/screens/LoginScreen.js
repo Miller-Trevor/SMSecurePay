80a9ac56977db74284464f64722b9fb92bed437e
Xavier Espinoza <102778178+xavieresp1@users.noreply.github.com>
2023-10-20 16:23:47 -0400

    Login hashing

--- navigation/screens/LoginScreen.js
+++ navigation/screens/LoginScreen.js
@@ -1,54 +1,83 @@
-import React , {useContext} from 'react';
+import * as React from 'react';
 import {SafeAreaView, View, TextInput, Text} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { AuthContext } from '../../context/AuthContext';
 import * as SQLite from 'expo-sqlite';
 import {useState, useEffect} from 'react';
+import * as Crypto from 'expo-crypto'
 
-export default function LoginScreen({navigation}) {
+
+export default function BuyerAccountScreen({navigation}) {
     const db = SQLite.openDatabase('main.db');
     const[password, setPassword] = useState();
     const[email, setEmail] = useState();
 
+    const SHA256Digest = async (input) => {
+        console.log("Digesting password into SHA256...");
+        try {
+          const hash = await Crypto.digestStringAsync(
+            Crypto.CryptoDigestAlgorithm.SHA256,
+            input
+          );
+          console.log('Hash of password: ' + hash);
+          return hash;
+        } catch (error) {
+          console.error('Error calculating SHA-256 hash:', error);
+          throw error;
+        }
+      };
 
-    useEffect(() => {
-        db.transaction(tx =>{
-            tx.executeSql('SELECT * FROM Accounts'),
-            (_,results)=>
-            {
-                console.log(results);
-            }
-        });
-    }, []);
-    const TryLogIn = () => {
-        db.transaction((tx) => {
+      const TryLogIn = async () => {
+        console.log("Attempting log in with given email and password...");
+        console.log('email: '+ email + ' Password: ' + password);
+        try {
+          console.log("calling hash function...")
+          const hashedPassword = await SHA256Digest(password);
+    
+          db.transaction((tx) => {
             tx.executeSql(
-                'SELECT * FROM Accounts WHERE email = ?',
-                [email],
-                (_, results) => {
-                    const len = results.rows.length;
-                    if (len === 1) {
-                        const user = results.rows.item(0);
-                        if (user.password === password) {
-                            // Password matches, log in successful
-                            console.log('Login successful');
-
-                            // Navigate to Account screen with email as a parameter
-                            navigation.navigate('Account', { userEmail: email });
-                        } else {
-                            // Password doesn't match
-                            console.error('Incorrect password');
-                        }
-                    } else if (len === 0) {
-                        // Email not found
-                        console.error('Email not found');
-                    } else {
-                        // More than one user with the same email (should not happen)
-                        console.error('Multiple users with the same email');
-                    }
+              'SELECT * FROM Accounts WHERE email = ?',
+              [email],
+              (_, results) => {
+                const len = results.rows.length;
+                if (len === 1) {
+                  const user = results.rows.item(0);
+                  if (user.password === hashedPassword) {
+                    // Password matches, log in successful
+                    console.log('Login successful');
+    
+                    // Navigate to Account screen with email as a parameter
+                    navigation.navigate('Account', { userEmail: email });
+                  } else {
+                    // Password doesn't match
+                    console.error('Incorrect password');
+                  }
+                } else if (len === 0) {
+                  // Email not found
+                  console.error('Email not found');
+                } else {
+                  // More than one user with the same email (should not happen)
+                  console.error('Multiple users with the same email');
                 }
+              }
             );
+          });
+        } catch (error) {
+          console.error('Error while hashing password:', error);
+        }
+      };
+
+    const DeleteAllAccountsFromDb = () =>{
+        db.transaction((tx) =>{
+            tx.executeSql('DELETE FROM Accounts;',[],
+            (tx, results) =>{
+                if(results.rowsAffected >= 1){
+                    console.log('Accounts deleted. ');
+                }
+            },
+        
+            );
+            
         });
     };
 
@@ -112,6 +141,13 @@ export default function LoginScreen({navigation}) {
             <TouchableOpacity onPress={() =>TryLogIn()} style={{backgroundColor:'#42EC95', padding:20, borderRadius:10, marginBottom:30}}>
                 <Text style={{textAlign:'center', fontWeight:'700', fontSize:16, color:'#fff'}}>
                     Login
+                </Text>
+            </TouchableOpacity>
+            </View>
+            <View style={{paddingLeft: 50, paddingRight:50}}>
+            <TouchableOpacity onPress={() =>DeleteAllAccountsFromDb()} style={{backgroundColor:'#42EC95', padding:20, borderRadius:10, marginBottom:30}}>
+                <Text style={{textAlign:'center', fontWeight:'700', fontSize:16, color:'#fff'}}>
+                    Remove all accounts from the db
                 </Text>
             </TouchableOpacity>
             </View>

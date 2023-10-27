e1e23247abe68868a7961b3894c7d050c62a5393
Miller-Trevor <131202550+Miller-Trevor@users.noreply.github.com>
2023-10-27 10:06:38 -0400

    Update OnboardingScreen.js - New Onboarding Style

--- navigation/screens/OnboardingScreen.js
+++ navigation/screens/OnboardingScreen.js
@@ -1,30 +1,58 @@
 import React from 'react'
 import { SafeAreaView, View, Text, TouchableOpacity } from 'react-native'
-
+import Onboarding from 'react-native-onboarding-swiper';
+import LottieView from "lottie-react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from 'expo-router';
 
 export default function OnboardingScreen({navigation}) {
-    return (
-        <SafeAreaView
-        style={{
-            flex:1,
-            justifyContent:'center',
-            alignItems:'center',
-            backgroundColor:'#fff',
-        }}
-        >
-            <View style={{marginTop:20}}>
-                <Text style={{fontWeight:'bold', fontSize:30, color:'#42EC95'}}>
-                    Welcome to SMSecurePay
-                </Text>
-            </View>
-            <TouchableOpacity style={{backgroundColor: '#42EC95', padding: 20, width:'90%', borderRadius:10, marginBottom:50, flexDirection:'row', justifyContent:'space-between'}} onPress={() => navigation.navigate('Register')}>
-                <Text style={{color:'white', fontSize:18, textAlign:'center', fontWeight:'bold'}}>
-                    Let's Start
-                </Text>
-                <Ionicons name="arrow-forward" size={22} color="#fff"/>
-            </TouchableOpacity>
 
+    const handleDone = () =>{
+        navigation.navigate('AppStack', {screen: "Home"});
+    }
+
+
+    return (
+        <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
+            <Onboarding
+                onDone={handleDone}
+                onSkip={handleDone}
+                containerStyles={{padding: 15}}
+                pages={[
+                    {
+                        backgroundColor: '#fff',
+                        image: (
+                            <View style={{width:300, height:400}}>
+                                <LottieView source={require("../../assets/animations/animation_lnxoe0by.json")} autoPlay loop />
+                            </View>
+                        ),
+                        title: 'Welcome to SMSecure Pay!',
+                        subtitle: 'Tap Next to Learn More and Start!'
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: (
+                            <View>
+                                <Text>Onboarding</Text>
+                            </View>
+                        ),
+                        title: 'SMSecure Pay is a Mimic Cash App',
+                        subtitle: 'The Intended Functionality is the Same as a Cash App and is not Intended to be Used on the Market',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: (
+                            <View>
+                                <Text>Onboarding</Text>
+                            </View>
+                        ),
+                        title: 'Start Now!',
+                        subtitle: 'Click next to Get Started with Account Creation!'
+                    },
+                
+                ]}
+            />
         </SafeAreaView>
     );
 };
+

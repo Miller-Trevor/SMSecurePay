b8bcfc51299c4cc0d3291282e5f1b037c625e833
Miller-Trevor <131202550+Miller-Trevor@users.noreply.github.com>
2023-11-17 16:32:18 -0500

    Delete Globals.js - moved to frontend

--- Globals.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalState } from "react-hooks-global-state";
-
-const {setGlobalState, useGlobalState} = createGlobalState({
-    loggedInUser: 'Guest',
-    UserID: 0
-});
-
-export{useGlobalState, setGlobalState};

309545a399c6fe621ffef716dfeb6f31b2c0f493
Miller-Trevor <131202550+Miller-Trevor@users.noreply.github.com>
2023-11-17 16:35:29 -0500

    Update FormScreen.js

--- SMSecureFrontend/navigation/screens/FormScreen.js
+++ SMSecureFrontend/navigation/screens/FormScreen.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {Text, TextInput, View, Button, SafeAreaView, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import {useState, useEffect} from 'react';
+import * as SQLite from 'expo-sqlite';
+import { useGlobalState, setGlobalState } from '../../Globals';
 
 import * as SMS from 'expo-sms';
 
@@ -8,11 +10,12 @@ import * as SMS from 'expo-sms';
 export default function FormScreen({navigation}){
 
     const [mobileNumber, setMobileNumber] = useState('');
+    const [email] = useGlobalState('loggedInUser');
     const [amount, setAmount] = useState('');
-    const [cardNumber, setCardNumber] = useState('');
+    const [cardNumber, setCardNumber] = useState('')
     const [isAvailable, setIsAvailable] = useState(false);
-    const [loading, setLoading] = useState(false);
-
+    const db = SQLite.openDatabase('main.db');
+    
 
     useEffect( () => {
         async function checkAvailability() {
@@ -20,20 +23,36 @@ export default function FormScreen({navigation}){
             setIsAvailable(isAvailable);
         }
         checkAvailability();
+        db.transaction(tx =>{
+            tx.executeSql('CREATE TABLE IF NOT EXISTS Transactions (ID INTEGER PRIMARY KEY AUTOINCREMENT, Sender TEXT, Receiver TEXT, Amount REAL);');
+        });
     }, []);
 
-    /* const DismissKeyboard = ({children}) => (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            {children}
-        </TouchableWithoutFeedback>
-    )
- */
     const sendSms = async () => {
-
-        /* if(mobileNumber != 10){
-            alert('You must enter a 10 digit phone number');
-            return;
-        } */
+        try {
+            db.transaction((tx) => {
+              tx.executeSql(
+                'INSERT INTO Transactions (Sender, Receiver, Amount) VALUES (?, ?, ?);',
+                [email, mobileNumber, amount],
+                (tx, results) => {
+                  console.log('SQL executed');
+      
+                  if (results.rowsAffected > 0) {
+                    // Insertion was successful
+                    console.log('Transaction saved to transaction history in DB');
+                  } else {
+                    // Insertion failed
+                    console.error('Failed to save the transaction');
+                  }
+                },
+                (error) => {
+                  console.error('Error during SQL execution:', error);
+                }
+              );
+            });
+          } catch (error) {
+            console.error('Error while saving transaction', error);
+          }
 
         let tempAmount = "Amount: " + amount;
         let tempCardNumber = "\nCard Number: " + cardNumber;
@@ -55,8 +74,11 @@ export default function FormScreen({navigation}){
     return(
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <SafeAreaView style={{flex: 3, alignItems: 'center', justifyContent: 'space-evenly' }}>
+                <Text style={{ fontWeight: 'bold', fontSize: 18, fontFamily: 'Thonburi-Bold' }}>
+                    {email}
+                </Text>
                 <Text style={{fontSize: 20, fontFamily: 'Thonburi-Bold'}}>
-                    Please Fill Out The Following Fields:
+                Please Fill Out The Following Fields:
                 </Text>
                 <View style={{}}>
                     <TextInput value={mobileNumber} onChangeText={
@@ -74,8 +96,7 @@ export default function FormScreen({navigation}){
                     } style={{backgroundColor: '#1632C1', width:200, padding:10, borderRadius: 20}} keyboardType='numeric' placeholder="Customer Card Number"/>
                 </View>
                 <View>
-                    {/* {isAvailable ? <Button onPress={sendSms} title='Send Message'/> : <Text>No SMS Available</Text>} */}
-                    <Button onPress={() => navigation.navigate('Payment')} title='Pay Now' />
+                    {isAvailable ? <Button onPress={sendSms} title='Send Message'/> : <Text>No SMS Available</Text>}
                 </View>
             </SafeAreaView>
         </TouchableWithoutFeedback>

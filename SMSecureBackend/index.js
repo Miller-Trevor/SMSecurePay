dfd52b51e2989c8bf1c676fc570ce33218ed38e1
Miller-Trevor <131202550+Miller-Trevor@users.noreply.github.com>
2023-11-30 16:20:30 -0500

    Update index.js

--- SMSecureBackend/index.js
+++ SMSecureBackend/index.js
@@ -1,68 +1,31 @@
-require("dotenv").config();
-const express = require("express");
+import express from "express";
+
 const app = express();
-const Stripe = require("stripe");
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-const cors = require("cors");
-const PORT = 3000;
+const port = 3000;
+const PUBLISHABLE_KEY = "pk_test_51N0q1vEnSZgQUDu6HZgM9mXP2t0K02f8cv6fD0Y7UV8G61iw1JPWmtyNF5foXm7scc2QVp88j464dS5AVKsubhmy00PeOQhLwX";
+import Stripe from 'stripe';
+const stripe = Stripe('sk_test_51N0q1vEnSZgQUDu6NLHP8hngXxf7TYtUqM0VvC1sbIqdF4cxtnriYxfqHrjBApmiBE11ZUK9WhyS8c5FO8arPt1000QHvVnXer', {apiVersion:"2023-10-16"})
 
-app.use(express.json());
-app.use(cors());
+app.listen(port, () => {
+  console.log(`App listening at http://172.20.10.5:${port}`);
+});
 
-app.post("/pay", async (req, res) => {
-    try {
-      // Getting data from client
-      let { amount, number } = req.body;
-      // Simple validation
-      if (!amount || !number)
-        return res.status(400).json({ message: "All fields are required" });
-      amount = parseInt(amount);
-      // Initiate payment
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: Math.round(amount * 100),
-        currency: "USD",
-        payment_method_types: ["card"],
-        metadata: { number },
-      });
-      // Extracting the client secret 
-      const clientSecret = paymentIntent.client_secret;
-      // Sending the client secret as response
-      res.json({ message: "Payment initiated", clientSecret });
-    } catch (err) {
-      // Catch any error and send error 500 to client
-      console.error(err);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  });
+app.post("/create-payment-intent", async(req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: "usd",
+      payment_method_types: ["card"],  
+    });
 
-  app.use("/stripe", express.raw({type: "*/*"}));
+    const clientSecret = paymentIntent.client_secret;
 
-  app.post("/stripe", async (req, res) => {
-    const sig = req.headers["stripe-signature"];
-    let event;
-
-    try {
-        event = await stripe.webhooks.constructEvent(
-            req.body,
-            sig,
-            process.env.STRIPE_WEBHOOK_SECRET
-        );
-    } catch (err) {
-        console.log(err);
-        return res.status(400).json({message:err.message});
-
-    }
-
-    if (event.type === "payment_intent.created") {
-        console.log(`${event.data.object.metadata.number} initated payment!`);
-      }
-      // Event when a payment is succeeded
-      if (event.type === "payment_intent.succeeded") {
-        console.log(`${event.data.object.metadata.number} succeeded payment!`);
-        // fulfilment
-      }
-      res.json({ ok: true });
-  });
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    res.json({
+      clientSecret: clientSecret,
+    })
+  } catch (e) {
+    console.log(e.message);
+    res.json({error: e.message});
+  }
+})
 
